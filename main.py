59891dbb9d2c332a52baf80ab6649f13bd5da0c0
etienne.dunnsigouin@tryg.no <duneti@duneti2204pd.prd1.prdroot.net>
2025-06-19 11:34:03 +0000

    my first push

---
+++ main.py
@@ -0,0 +1,9 @@
+from etienne import config
+
+def main():
+    print("Hello from etienne!")
+
+
+if __name__ == "__main__":
+    main()
+    print(config.path_in)
---
+++ src/etienne/config.py
@@ -0,0 +1,1 @@
+path_in='blablabla'
